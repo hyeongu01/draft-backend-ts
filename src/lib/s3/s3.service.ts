1e47da0809d71f14e63ad1e3e2c2c6083240659a
hyeongu01 <cdy5212@gmail.com>
2026-06-11 17:56:04 +0900

    feat: 파일 업로드 서비스 구조 확립

--- src/lib/s3/s3.service.ts
+++ src/lib/s3/s3.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import { S3Client } from '@aws-sdk/client-s3';
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import CONFIG from '@/config/config';
 
 @Injectable()
 export class S3Service {
-  public client = new S3Client({
+  private readonly client = new S3Client({
     region: 'auto',
     endpoint: `https://${CONFIG.r2.accountId}.r2.cloudflarestorage.com`,
     credentials: {
@@ -12,4 +12,19 @@ export class S3Service {
       secretAccessKey: CONFIG.r2.secretAccessKey,
     },
   });
+
+  async uploadProfileImage(
+    file: Express.Multer.File,
+    fileName: string,
+  ): Promise<string> {
+    await this.client.send(
+      new PutObjectCommand({
+        Bucket: CONFIG.r2.bucketName,
+        Key: fileName,
+        Body: file.buffer,
+        ContentType: file.mimetype,
+      }),
+    );
+    return `${CONFIG.r2.publicUrl}/${fileName}`;
+  }
 }
--- src/modules/files/files.controller.ts
+++ src/modules/files/files.controller.ts
@@ -6,8 +6,6 @@ import {
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { S3Service } from '@/lib/s3/s3.service';
-import { PutObjectCommand } from '@aws-sdk/client-s3';
-import CONFIG from '@/config/config';
 
 @Controller('files')
 export class FilesController {
@@ -16,14 +14,10 @@ export class FilesController {
   @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
   async uploadFile(@UploadedFile() file: Express.Multer.File) {
-    const obj = new PutObjectCommand({
-      Bucket: CONFIG.r2.bucketName,
-      Key: file.originalname,
-      Body: file.buffer,
-      ContentType: file.mimetype,
-    });
-    await this.s3Service.client.send(obj);
-
-    return { url: `${CONFIG.r2.publicUrl}/${file.originalname}` };
+    const url = await this.s3Service.uploadProfileImage(
+      file,
+      file.originalname,
+    );
+    return { url };
   }
 }
